87d8141944a505d4a432f281cdabdee025739c0d
Young <willy4202@naver.com>
2022-07-06 15:48:13 +0900

    add: 코어자바스크립트트 this part2

--- Javascript/this.js
+++ Javascript/this.js
@@ -61,12 +61,84 @@
 
 // 생성자 함수
 
-var Cat = function (name, age) {
-  this.bark = "야옹";
-  this.name = name;
-  this.age = age;
-};
+// var Cat = function (name, age) {
+//   this.bark = "야옹";
+//   this.name = name;
+//   this.age = age;
+// };
 
-var choco = new Cat("초코", 7);
-var nabi = new Cat("나비", 4);
-console.log(choco, nabi);
+// var choco = new Cat("초코", 7);
+// var nabi = new Cat("나비", 4);
+// console.log(choco, nabi);
+
+// var func = function (a, b, c) {
+//   console.log(this, a, b, c);
+// };
+
+// func(1, 2, 3);
+// func.call({ x: 1 }, 4, 5, 6);
+
+// var obj = {
+//   a: 1,
+//   method: function (x, y) {
+//     console.log(this.a, x, y);
+//   },
+// };
+
+// obj.method(2, 3);
+// obj.method.call({ a: 4 }, 5, 6);
+
+// var func = function (a, b, c) {
+//   console.log(this, a, b, c);
+// };
+// func.apply({ x: 1 }, [4, 5, 6]);
+
+// var obj = {
+//   a: 1,
+//   method: function (x, y) {
+//     console.log(this.a, x, y);
+//   },
+// };
+// obj.method.apply({ a: 4 }, [5, 6]);
+
+// var obj = {
+//   0: "a",
+//   1: "b",
+//   2: "c",
+//   length: 3,
+// };
+
+// Array.prototype.push.call(obj, "d");
+// console.log(obj);
+
+// var arr = Array.prototype.slice.call(obj);
+// console.log(arr);
+
+// var obj = {
+//   0: "a",
+//   1: "b",
+//   2: "c",
+//   length: 3,
+// };
+
+// var arr = Array.from(obj);
+// console.log(arr);
+
+// function Person(name, gender) {
+//   this.name = name;
+//   this.gender = gender;
+// }
+
+// function Student(name, gender, school) {
+//   Person.call(this, name, gender);
+//   this.school = school;
+// }
+
+// function Employee(name, gender, company) {
+//   Person.apply(this, [name, gender]);
+//   this.company = company;
+// }
+// var by = new Student("보영", "female", "숙명여대");
+// var jn = new Employee("재난", "male", "구글");
+
+// console.log(by, jn);
