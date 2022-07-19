a8354adaaebbcb756f8e883bed00466b77bca512
Young <willy4202@naver.com>
2022-07-19 23:38:54 +0900

    add: 이름변경

--- 독서/코어자바스크립트/콜백함수.js
+++ 독서/코어자바스크립트/콜백함수.js
@@ -4,10 +4,23 @@
 //   if (++count > 4) clearInterval(timer);
 // }, 300);
 
-var count = 0;
-var cbFunc = function () {
-  console.log(count);
-  if (++count > 4) clearInterval(timer);
-};
-var timer = setInterval(cbFunc, 300);
-ㄴㄴ;
+// var count = 0;
+// var cbFunc = function () {
+//   console.log(count);
+//   if (++count > 4) clearInterval(timer);
+// };
+// var timer = setInterval(cbFunc, 300);
+
+// var newArr = [10, 20, 30].map(function (current, index) {
+//   console.log(current, index);
+//   return current + 5;
+// });
+
+// console.log(newArr);
+
+function Liniter(n) {
+  if (n == 6) return 3;
+  else return 3 * Liniter(n + 1);
+}
+
+console.log(Liniter(3));
